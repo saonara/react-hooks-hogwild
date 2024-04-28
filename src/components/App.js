d238237a06b6364c6ffc9b4f845ea16ad92c76b8
odongo <saonarawestinc@outlook.com>
2024-04-28 08:05:07 +0300

    Initial commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,19 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Nav from "./Nav";
 
 import hogs from "../porkers_data";
 
 function App() {
-	return (
-		<div className="App">
-			<Nav />
-		</div>
-	);
+  useEffect(() => {
+    const data = hogs;
+    console.log(data);
+  }, []);
+
+  return (
+    <div className="App">
+      <Nav />
+    </div>
+  );
 }
 
 export default App;
